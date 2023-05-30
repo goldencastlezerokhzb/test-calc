93362a37240311574ee63e6e9ed81f2bab6fd1d8
goldencastlezerokhzb <oltinqala3@mail.ru>
2023-05-30 15:38:06 +0500

    initial commit added calc and added strong password regex

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -6,5 +9,10 @@ public class Main {
         int b = 2;
         int c = a + b;
         System.out.println(c);
+        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$");
+        Matcher matcher = pattern.matcher("987654rtQWERTY+_");
+        System.out.println("matcher.matches() = " + matcher.matches());
+
+
     }
 }
