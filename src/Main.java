2ebdb3c8aa4726641217d3578d17aad74f5077d1
goldencastlezerokhzb <oltinqala3@mail.ru>
2023-05-30 15:28:08 +0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello test project!");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
     }
 }
